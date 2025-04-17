6b27e2125abc5173668d7c16c9c91be5f33b3fa9
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-04-16 23:08:15 -0500

    Intento de corrección de temblor 1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardGridComponent } from "../card-grid/card-grid.component";
 import { PublicoService } from '../../services/publico.service';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -12,7 +12,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  carouselItems = [
+    {
+      imageUrl: 'https://www.semana.com/resizer/v2/UBZ5DYABJZCQVMS26LBZDA2QTM.jpg?auth=394188381b20bf2c9faf8c2d44e30e5b6240c03549840bb27f596246abfe2d7c&smart=true&quality=75&width=1280&height=720',
+      title: 'Bienvenido a Tienda Sana',
+      subtitle: 'Descubre productos saludables para tu estilo de vida'
+    },
+    {
+      imageUrl: 'https://strapi.fitia.app/uploads/verduras_402de1696c.jpg',
+      title: 'Ofertas Especiales',
+      subtitle: 'Aprovecha descuentos exclusivos en nuestros productos'
+    },
+    {
+      imageUrl: 'https://ccviva.com/sites/default/files/2024-09/restaurantes-envigado.jpeg',
+      title: 'Calidad Garantizada',
+      subtitle: 'Productos seleccionados con los más altos estándares'
+    }
+  ];
+
   currentPage: number = 0;
   filterForm!: FormGroup;
   productos: any[] = [];
@@ -21,33 +39,38 @@ export class HomeComponent {
   pages: number[] = [];
   filterUsed: boolean = false;
   tipos: string[] = [];
-
-  carouselItems = [
-    {
-      imageUrl: "https://www.muyinteresante.com/wp-content/uploads/sites/5/2023/08/16/64dce698e98ce.jpeg",
-      title: "Encuentra tu comida ideal",
-      subtitle: "Descubre los alimentos que se adaptan a tu estilo de vida"
-    },
-    {
-      imageUrl: "https://www.flora.es/-/media/Project/Upfield/Brands/Becel-NL/Flora-ES/Assets/Cuidate/header-10-alimentos-saludables-que-son-buenos-para-tu-corazon.jpg?rev=92f792036c7f4ef7b5525cee92989a63",
-      title: "Bebidas increíbles",
-      subtitle: "Un deleite para tu paladar"
-    },
-    {
-      imageUrl: "https://agenciadenoticias.unal.edu.co/fileadmin/Agencia_de_Noticias/Imagenes/2022/08-Agosto/395/AgenciaUNAL-01-1008.jpg",
-      title: "Tu próxima reserva",
-      subtitle: "Ofrecemos el mejor servicio"
-    }
-  ];
-
   activeSlideIndex: number = 0;
+  slideInterval: any; // Variable para almacenar el temporizador
+
 
   constructor(private publicoService: PublicoService, private formBuilder: FormBuilder) {
     this.productos = [];
     this.obtenerProductos(this.currentPage);
     this.obtenerCategorias();
-    this.createForm();
+    
   }
+
+  ngOnInit(): void {
+    this.createForm();
+    this.startSlideInterval(); // Inicia el cambio automático de subtítulos
+  }
+
+  ngOnDestroy(): void {
+    this.stopSlideInterval(); // Detiene el temporizador al destruir el componente
+  }
+
+  startSlideInterval(): void {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Cambia cada 5 segundos
+  }
+
+  stopSlideInterval(): void {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
 
   nextSlide() {
     this.activeSlideIndex = (this.activeSlideIndex + 1) % this.carouselItems.length;
