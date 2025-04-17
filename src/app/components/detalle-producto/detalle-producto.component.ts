ea261da29923141c153ddf7e9ccf08921717b764
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2025-04-17 15:29:16 -0500

    Update detalle-producto.component.ts

--- src/app/components/detalle-producto/detalle-producto.component.ts
+++ src/app/components/detalle-producto/detalle-producto.component.ts
@@ -1,39 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoDTO } from '../../dto/producto-dto';
 import { ItemCarritoDTO } from '../../dto/item-carrito-dto';
+import { FormGroup, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-detalle-producto',
   standalone: true,
   templateUrl: './detalle-producto.component.html',
-  styleUrls: ['./detalle-producto.component.css']
+  styleUrls: ['./detalle-producto.component.css'],
+  imports: [CommonModule, FormsModule],
 })
 export class DetalleProductoComponent  {
   producto?: ProductoDTO;
   itemCarrito?: ItemCarritoDTO;
-  cantidadSeleccionada: number = 1;
+  detalleCarrtitoForm: FormGroup;
 
-  // Propiedades calculadas
-  productoCantidad: number = 0;
-  productoPrecio: string = '';
-  productoPrecioOriginal: string = '';
-  productoDescuento: number | null = null;
-  productoCalificacion: number = 0;
-  productoResenias: number | null = null;
-  estadoProducto: string = '';
-  estadoProductoClass: string = '';
-  cantidadTexto: string = '';
-  cantidadClass: string = '';
-  cantidadIconClass: string = '';
+  constructor() {
+  }
+
 
   
 
-  agregarAlCarrito(): void {
-    console.log(`Agregado al carrito: ${this.cantidadSeleccionada} unidades de ${this.producto?.nombre}`);
-  }
-
-  agregarAFavoritos(): void {
-    console.log(`Producto agregado a favoritos: ${this.producto?.nombre}`);
-  }
+  
 }
