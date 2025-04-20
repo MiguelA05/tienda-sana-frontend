3064f88b651261a5307083c694cb7916d71587c8
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-04-19 23:14:03 -0500

    Documentación servicios parte 1

--- src/app/guardias/permiso.service.ts
+++ src/app/guardias/permiso.service.ts
@@ -7,7 +7,19 @@ import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } fr
 })
 export class PermisoService {
 
+  /**
+   * Constructor de la clase PermisoService
+   * @param tokenService tokenService para manejar el token de autenticación
+   * @param router router para navegar entre rutas
+   */
   constructor(private tokenService: TokenService, private router: Router) { }
+
+  /**
+   * Método para verificar si el usuario tiene permiso para acceder a la ruta
+   * @param next ruta activa
+   * @param state estado de la ruta
+   * @returns true si el usuario no está autenticado, false en caso contrario
+   */
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.tokenService.isLogged()) {
     this.router.navigate([""]);
@@ -16,6 +28,13 @@ export class PermisoService {
     return true;
     }
 }
+
+/**
+ * Componente para verificar si el usuario tiene permiso para acceder a la ruta
+ * @param next nueva ruta activa
+ * @param state estado de la ruta
+ * @returns true si el usuario no está autenticado, false en caso contrario
+ */
 export const LoginGuard: CanActivateFn = (next: ActivatedRouteSnapshot, state:
   RouterStateSnapshot): boolean => {
   return inject(PermisoService).canActivate(next, state);
--- src/app/services/auth.services.ts
+++ src/app/services/auth.services.ts
@@ -17,34 +17,67 @@ export class AuthService {
   private authURL = "http://localhost:8080/api/auth";
 
   constructor(private http: HttpClient) {
-
-
   }
+
+  /**
+   * Metodo para crear una cuenta de usuario
+   * @param cuentaDTO cuentaDTO con los datos de la cuenta a crear
+   * @returns respuesta del servidor
+   */
   public crearCuenta(cuentaDTO: CrearCuentaDTO): Observable<MensajeDTO> {
     return this.http.post<MensajeDTO>(`${this.authURL}/create-account`, cuentaDTO);
   }
 
+  /**
+   * Metodo para iniciar sesion
+   * @param loginDTO loginDTO con los datos de inicio de sesion
+   * @returns respuesta del servidor
+   */
   public iniciarSesion(loginDTO: LoginDTO): Observable<MensajeDTO> {
     return this.http.post<MensajeDTO>(`${this.authURL}/login`, loginDTO);
   }
 
+  /**
+   * Metodo para refrescar el token de acceso
+   * @param token tokenDTO con el token de refresco
+   * @returns respuesta del servidor
+   */
   public refresh(token: TokenDTO): Observable<MensajeDTO> {
     return this.http.post<MensajeDTO>(`${this.authURL}/refresh`, token);
   }
 
+  /**
+   * Metodo para validar el codigo de registro
+   * @param activateAccountDTO DTO con los datos de la cuenta a activar
+   * @returns respuesta del servidor
+   */
   public validarCodigoRegistro(activateAccountDTO: ActivarCuentaDTO): Observable<MensajeDTO> {
     return this.http.put<MensajeDTO>(`${this.authURL}/validate-account`, activateAccountDTO);
   }
 
+  /**
+   * Metodo para enviar el codigo de recuperacion de contrasenia
+   * @param email email del usuario a recuperar
+   * @returns respuesta del servidor
+   */
   public enviarCodigoRecuperacion(email: string): Observable<MensajeDTO> {
     return this.http.put<MensajeDTO>(`${this.authURL}/send-recover/${email}`, {});
   }
 
-  //Prueba
+  /**
+   * Metodo para cambiar la contrasenia del usuario
+   * @param cambiarContra DTO DTO con los datos de la contrasenia a cambiar
+   * @returns respuesta del servidor
+   */
   public cambiarContrasenia(cambiarContra: CambiarContraseniaDTO): Observable<MensajeDTO> {
     return this.http.put<MensajeDTO>(`${this.authURL}/change-password`, cambiarContra);
   }
 
+  /**
+   * Metodo para reenviar el codigo de verificacion al email
+   * @param email email del usuario a recuperar
+   * @returns respuesta del servidor
+   */
   public reenviarCodigoVerificacion (email: string): Observable<MensajeDTO> {
     return this.http.put<MensajeDTO>(`${this.authURL}/resend-validation/${email}`, {});
   }
