40445e367060e59bd02a41605c4f9be07faa7894
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-05-06 21:20:05 -0500

    Eliminar archivos de entorno del seguimiento de Git

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  authServiceUrl: 'http://localhost:8080/api/auth',
-  clienteServiceUrl: 'http://localhost:8080/api/cliente',
-  cuentaServiceUrl: 'http://localhost:8080/api/account',
-  publicoServiceUrl: 'http://localhost:8080/api/public',
-};
